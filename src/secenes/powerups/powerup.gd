c4d00b94575d22ef34876ee6bc33251d143b68de
Giani Statie <44785388+GianiStatie@users.noreply.github.com>
2025-11-17 17:36:57 +0200

    made tiles distructable on stomp

--- src/secenes/powerups/powerup.gd
+++ src/secenes/powerups/powerup.gd
@@ -4,4 +4,5 @@ extends Area2D
 
 func get_powerup() -> String:
 	var powerup_names = Constants.POWERUPS.keys()
-	return powerup_names.pick_random()
+	#return powerup_names.pick_random()
+	return "Stomp"
