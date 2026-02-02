9bb14cbb7ca4713d59c1e4d1a5e4f65c0e35dff9
Giani Statie <44785388+GianiStatie@users.noreply.github.com>
2026-02-02 16:52:07 +0200

    fix end screen progress

--- src/ui/menus/end_screen.gd
+++ src/ui/menus/end_screen.gd
@@ -4,6 +4,8 @@ extends MarginContainer
 @onready var level_name_label = %LevelNameLabel
 @onready var current_time_label = %CurrentTimeLabel
 @onready var best_time_label = %BestTimeLabel
+@onready var preogress_bar = %LevelProgressBar
+@onready var progress_medal = %LevelProgressMedalLabel
 @onready var leaderboard = %Leaderboard
 
 
@@ -14,7 +16,12 @@ func update_stats(stats: Dictionary) -> void:
 	
 	current_time_label.text = Utils.format_time(stats["time"])
 	
-	var level_data = SaveManager.get_level_data(stats["level_name"])
+	var level_data: LevelData = SaveManager.get_level_data(stats["level_name"])
+	var your_best_time = "Not Done Yet" if level_data.best_time == INF else Utils.format_time(level_data.best_time)
+	best_time_label.text = your_best_time
+	preogress_bar.value = level_data.progress_percentage
+	progress_medal.text = Constants.MEDAL_NAMES[level_data.progress_milestone]
+	
 	best_time_label.text = Utils.format_time(level_data.best_time)
 	
 	new_best_label.visible = level_data.best_time == stats["time"]
